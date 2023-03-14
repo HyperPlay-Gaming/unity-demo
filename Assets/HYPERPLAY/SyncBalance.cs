cf5ac31dce9fd607386644c51bff27d449a20013
leon-do <leondo123@gmail.com>
2023-03-14 13:02:48 -0400

    callContract

--- Assets/HYPERPLAY/SyncBalance.cs
+++ Assets/HYPERPLAY/SyncBalance.cs
@@ -13,12 +13,8 @@ public class SyncBalance : MonoBehaviour
 
     async public void OnSyncBalance()
     {
-        // https://docs.gaming.chainsafe.io/erc20#balance-of_3
-        string chain = "ethereum";
-        string network = "goerli";
-        string contract = "0xDD6ff2bA7fD02D19e8e4e1d99b65802eD9705437";
         string account = await GetAccount();
-        BigInteger balanceOf = await ERC20.BalanceOf(chain, network, contract, account);
+        BigInteger balanceOf = await GetERC20Balance(account);
 
         // display on button
         PlayerBalance.text = "Sync Balance: " + balanceOf.ToString();
@@ -32,6 +28,23 @@ public class SyncBalance : MonoBehaviour
         {
             Player.GetComponent<Renderer>().material.color = Color.white;
         }
+    }
+
+    async private static Task<BigInteger> GetERC20Balance(string _account) {
+        string jsonString = "{ \"contractAddress\": \"0xDD6ff2bA7fD02D19e8e4e1d99b65802eD9705437\", \"functionName\": \"balanceOf\", \"abi\": [{ \"inputs\": [{ \"internalType\": \"address\", \"name\": \"account\", \"type\": \"address\" }], \"name\": \"balanceOf\", \"outputs\": [{ \"internalType\": \"uint256\", \"name\": \"\", \"type\": \"uint256\" }], \"stateMutability\": \"view\", \"type\": \"function\" }], \"params\": [\"" + _account + "\"], \"chain\": { \"chainId\": \"5\" } }";
+        byte[] jsonBytes = System.Text.Encoding.UTF8.GetBytes(jsonString);
+
+        UnityWebRequest request = new UnityWebRequest("localhost:9680/callContract", "POST");
+        request.uploadHandler = new UploadHandlerRaw(jsonBytes);
+        request.downloadHandler = new DownloadHandlerBuffer();
+        request.SetRequestHeader("Content-Type", "application/json");
+
+        await request.SendWebRequest();
+        string response = request.downloadHandler.text; // "1"
+        print(response);
+        // convert to BigInteger
+        BigInteger balanceOf = BigInteger.Parse(response);
+        return balanceOf;
     }
 
     async private static Task<string> GetAccount() {
